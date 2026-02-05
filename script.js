e8b5e0a39f1ed10465896b00f9e9d2cd3520d921
Anshu Kaushik <kaushikanshu0108@gmail.com>
2026-02-05 13:45:02 +0530

    Implement dynamic event card creation

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const addBtn = document.getElementById("addEvent");
 const container = document.getElementById("eventsContainer");
+addBtn.addEventListener("click", function(){
+
+    const title = document.getElementById("title").value;
+    const date = document.getElementById("date").value;
+
+    const card = document.createElement("div");
+    card.innerHTML = `
+        <h3>${title}</h3>
+        <p>${date}</p>
+    `;
+
+    container.appendChild(card);
+});
