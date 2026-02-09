3a7097ccd3743e8f43085ae1886ed1d52ac4f066
Anshu Kaushik <kaushikanshu0108@gmail.com>
2026-02-09 16:26:59 +0530

    final code

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
-const addBtn = document.getElementById("addEvent");
-const eventsContainer = document.getElementById("eventsContainer");
+function addEvent(){
 
-addBtn.addEventListener("click", () => {
-
-    const title = document.getElementById("title").value.trim();
-    const date = document.getElementById("date").value;
-    const category = document.getElementById("category").value;
-    const description = document.getElementById("description").value.trim();
+    let title = document.getElementById("title").value;
+    let date = document.getElementById("date").value;
+    let category = document.getElementById("category").value;
+    let description = document.getElementById("description").value;
 
     if(title === "" || date === ""){
-        alert("Please enter event title and date");
+        alert("Please fill required fields!");
         return;
     }
 
-    const card = document.createElement("div");
+    let eventList = document.getElementById("eventList");
+
+    let card = document.createElement("div");
     card.className = "event-card";
 
     card.innerHTML = `
-        <div class="card-header">
-            <h3 class="event-title">${title}</h3>
-            <button class="delete-btn">X</button>
-        </div>
+        <button class="delete-btn" onclick="deleteEvent(this)">X Delete</button>
 
-        <p><strong>Date:</strong> ${date}</p>
-        <p><strong>Category:</strong> ${category}</p>
+        <div class="event-title">${title}</div>
+
+        <p><b>Date:</b> ${date}</p>
+        <p><b>Category:</b> ${category}</p>
         <p>${description}</p>
 
-        <button class="complete-btn">Mark Complete</button>
+        <button class="complete-btn" onclick="markComplete(this)">
+            Mark Complete
+        </button>
     `;
 
-    eventsContainer.appendChild(card);
+    eventList.appendChild(card);
 
     // Clear form
-    document.getElementById("title").value = "";
-    document.getElementById("date").value = "";
-    document.getElementById("description").value = "";
-});
+    document.getElementById("title").value="";
+    document.getElementById("date").value="";
+    document.getElementById("description").value="";
+}
 
-/* Event Delegation */
-eventsContainer.addEventListener("click", (e) => {
+function deleteEvent(button){
+    button.parentElement.remove();
+}
 
-    if(e.target.classList.contains("delete-btn")){
-        e.target.closest(".event-card").remove();
-    }
-
-    if(e.target.classList.contains("complete-btn")){
-        e.target.closest(".event-card").classList.toggle("completed");
-    }
-});
+function markComplete(button){
+    button.parentElement.classList.toggle("complete");
+}
